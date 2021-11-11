2e4308e6bd747b9cd91ef35d0ac93faa7a19ef91
fontilan <68748054+fontilan@users.noreply.github.com>
2021-11-11 12:28:13 +0100

    add text-shadow

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 // @ts-check
-
-let firstCard = 10;
-let secondCard = 7;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Wojtek",
+  chips: 200,
+};
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+  if (randomCard === 1) {
+    return 11;
+  } else if (randomCard >= 11) {
+    return 10;
+  } else return randomCard;
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -31,8 +51,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-  let card = 2;
-  sum += card;
-  renderGame();
+  if (isAlive && !hasBlackjack) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
